099f62dc4e7127d2bb401d0de901891946f33f7f
MittaliWalia <100557094+MittaliWalia@users.noreply.github.com>
2025-08-17 22:10:34 -0400

    Add files via upload

---
+++ Models/MedicalRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace HealthSphere_CapstoneProject.Models
+{
+    public class MedicalRecord
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Diagnosis { get; set; }
+        public string Prescription { get; set; }
+        public string Vitals { get; set; }
+    }
+
+}
