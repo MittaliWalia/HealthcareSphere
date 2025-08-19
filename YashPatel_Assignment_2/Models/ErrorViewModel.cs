64a3f588260982718902d7fa3782ce27d922de95
DevPtlYsh <ypsjs123@gmail.com>
2025-08-18 23:35:54 -0400

    Delete YashPatel_Assignment_2 directory

--- YashPatel_Assignment_2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace YashPatel_Assignment_2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
